2bb31191f7fb400489093782ad78ba9cb8ecb1d8
Joe Spampinato <jcspampinato@gmail.com>
2016-02-26 12:58:07 -0500

    Convert ratings to stars

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
-  devise_for :users
-  resources :movies
 
-  root to: "movies#index"
+	devise_for :users
+	resources :movies do
+	  resources :reviews, except: [:show, :index]
+	end
+
+	root "movies#index"
 
 end
